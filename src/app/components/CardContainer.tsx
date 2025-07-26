3855aeff48709b43e0f2ba9f6abb486404a31f57
skill-informatic <mariojhaziel04@gmail.com>
2025-07-25 21:15:12 -0600

    updated

--- src/app/components/CardContainer.tsx
+++ src/app/components/CardContainer.tsx
@@ -1,32 +1,54 @@
 import { Box, Card, CardContent, Grid, Typography } from "@mui/material";
 import { CardTypes } from "../models/globalInfo.types";
+
 interface Types {
   cards: CardTypes[];
+  withColumn?: false;
 }
-const CardContainer = ({ cards }: Types) => {
+
+const CardContainer = ({ cards, withColumn }: Types) => {
   return (
     <Grid
       sx={{
         display: "grid",
-        gridTemplateColumns: {
-          xs: "1fr 1fr",
-          sm: " 1fr",
-          md: " 1fr",
-          lg: " 1fr 1fr",
-          xl: " 230px 230px 230px",
-        },
+
+        gridTemplateColumns: withColumn
+          ? {
+              xs: "1fr 1fr",
+              sm: "1fr",
+              md: "1fr",
+              lg: "1fr 1fr",
+              xl: "230px 230px 230px",
+            }
+          : {
+              xs: "1fr ",
+              sm: "150px  ",
+              md: "200px 200px ",
+              lg: "1fr 1fr 1fr",
+              xl: "230px 230px 230px",
+            },
+
         justifyItems: {
           xs: "center",
           lg: "left",
         },
-        columnGap: {
-          xs: "3%",
-          md: "1.5%",
-          lg: "3%",
-          xl: "3%",
-        },
+        columnGap: withColumn
+          ? {
+              xs: "3%",
+
+              md: "1.5%",
+              lg: "3%",
+              xl: "3%",
+            }
+          : {
+              xs: "3%",
+              sm: "33%",
+              md: "10%",
+              lg: "3%",
+              xl: "3%",
+            },
         rowGap: {
-          xs: "3%",
+          xs: "5%",
         },
       }}
     >
@@ -44,38 +66,48 @@ const CardContainer = ({ cards }: Types) => {
             alignItems: "center",
             border: ".1rem hidden black",
             transition: "all .2s ease",
+            boxShadow: "3px 3px 4px 1px rgba(0, 0, 0, 0.2)",
             "&:hover": {
               transition: "all .2s ease",
               boxShadow: "4px 4px 5px 1px rgba(0, 0, 0, 0.4)",
             },
           }}
-          onClick={() => carta.action()}
+          onClick={() => carta.action(carta.id)}
         >
           <CardContent>
-            {carta.icono}
+            <Box
+              sx={{
+                backgroundColor: "#7E5BFF",
+                borderRadius: "50%",
+                width: "100px",
+                height: "100px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                margin: "0 auto 1rem",
+              }}
+            >
+              <Box sx={{ color: "#fff" }}>{carta.icono}</Box>
+            </Box>
+
             <Typography
               variant="h6"
               sx={{
                 color: "#767676",
-                fontSize: "1.2rem",
+                fontSize: "1.1rem",
               }}
             >
               {carta.name}
             </Typography>
-            <Box
-              component="div"
-              sx={{
-                width: "150px",
-              }}
-            >
+
+            <Box component="div" sx={{ width: "150px", margin: "0 auto" }}>
               <Typography
                 variant="subtitle2"
                 sx={{
                   fontWeight: "400",
                   lineHeight: ".9rem",
-                  //falta arreglar el open sans
-                  fontFamily: "Open sans",
-                  fontSize: ".9rem",
+                  fontFamily: "Open Sans",
+                  fontSize: ".8rem",
                   color: "#B0B0B0",
                 }}
               >
@@ -88,4 +120,5 @@ const CardContainer = ({ cards }: Types) => {
     </Grid>
   );
 };
+
 export default CardContainer;
