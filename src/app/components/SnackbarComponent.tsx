0382a0543ba9e6e7678cfac4151b485be71bc1cd
skill-informatic <mariojhaziel04@gmail.com>
2025-07-26 03:45:13 -0600

    alert component

---
+++ src/app/components/SnackbarComponent.tsx
@@ -0,0 +1,28 @@
+import { Alert, Snackbar } from "@mui/material";
+import React from "react";
+
+interface props {
+  open: boolean;
+  on_close: () => void;
+  type: "success" | "error" | "info" | "warning";
+  text: string;
+}
+
+export default function SnackbarComponent(props: props) {
+  return (
+    <Snackbar
+      onClose={props.on_close}
+      open={props.open}
+      anchorOrigin={{ vertical: "top", horizontal: "left" }}
+      autoHideDuration={4000}
+    >
+      <Alert
+        variant="filled"
+        severity={props.type}
+        sx={{ backgroundColor: "", color: "white", fontWeight: 900 }}
+      >
+        {props.text}
+      </Alert>
+    </Snackbar>
+  );
+}
